0b57790411b35de5f559c5570a8434656e524ff2
acb22ma <126529574+acb22ma@users.noreply.github.com>
2024-04-11 20:23:49 +0100

    adding machine readable and human readable output options

--- RSE.py
+++ RSE.py
@@ -4,15 +4,16 @@ Verison: 1.0
 Description: Script that interacts with the Ontology Lookup Service and uses their API to get 
 the data and display it to the user.
 """
+import json
 import requests
 
 #Taking the user Ontology ID as input and converting it to lowercase to avoid case sensitivity
 print("Welcome to the Ontology Lookup Service!")
-input = input("Please enter the Ontology ID: ")
-input = input.lower()
+id_input = input("Please enter the Ontology ID: ")
+id_input = id_input.lower()
 
 #Using the API to get the data from the Ontology Lookup Service
-url = "https://www.ebi.ac.uk/ols4/api/ontologies/" + input
+url = "https://www.ebi.ac.uk/ols4/api/ontologies/" + id_input
 response = requests.get(url)
 data = response.json()
 
@@ -24,8 +25,24 @@ status = data['status']
 terms = data['numberOfTerms']
 
 #Output the parsed data
-print("")
-print("Title:", title)
-print("Description:", description)
-print("Number of Terms:", terms)
-print("Status:", status)
+#Ask the user if they would like a machine-readable output
+mr_output = input("Would you like a machine-readable output? (yes/no): ")
+mr_output = mr_output.lower()
+if mr_output == "yes":
+    ontology_details = {
+            "title": title,
+            "description": description,
+            "number_of_terms": terms,
+            "status": status
+        }
+    print("")
+    print(json.dumps(ontology_details))
+    print("")
+else:
+    print("")
+    print("Title:", title)
+    print("Description:", description)
+    print("Number of Terms:", terms)
+    print("Status:", status)
+    print("")
+
